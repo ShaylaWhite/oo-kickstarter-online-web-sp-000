afa97164d40aff18c296a2cb631c4cdf1aa52d14
Shayla White <shayladwhite1@gmail.com>
2019-07-20 15:47:40 +0000

    Automatically backed up by Learn

---
+++ lib/project.rb
@@ -0,0 +1,16 @@
+class Project
+  
+  attr_reader :title
+  attr_accessor :backers
+  
+  def initialize(title)
+    @title = title
+    @backers = []
+  end
+  
+  def add_backer(backers)
+    @backers << backers
+    
+  end
+  
+end
